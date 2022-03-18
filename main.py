2cf3e03908c206eb0f7809982447b5613db62947
masterchlop <254661@student.pwr.edu.pl>
2022-03-18 23:53:37 +0100

    metoda rozpoczęcia

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 import generateData
+import nearestNeigbor2
+import nearestNeighbour
 from nearestNeighbour import *
 from nearestNeigbor2 import *
 from krandom import *
+import sys
+
+
+def write_info():
+    print("Instance name:", instance.name)
+    print("Dimention:", instance.size)
+    print("Distance Type:", instance.EdgeWeightType)
+
+
+def write_results():
+    # ścieżka
+    displayTour.printPath(dis_mat, path)
+    # dystans
+    print("rozwiązanie: ", calcTour.fc(dis_mat, path))
+    # wykres
+    if instance.getEdgeWeightType() == "EUC_2D":
+        displayTour.EUCgraph(instance, path)
 
 
 if len(sys.argv) == 1:
@@ -14,40 +33,48 @@ if len(sys.argv) == 1:
     maximum = input(" maximum: ")
     newFile = generateData
     newFile.generateData(int(dimension), int(seed), option, int(minimum), int(maximum))
-    # with open('TSP_Data/random_instance_file.tsp', 'r') as data:
-    #     pass
-    while True:
-        alg = input("NN, k-NN, k-random, 2opt, or anything else to quit ")
-        if alg == "NN":
-            t = nearestNeighbour('TSP_Data/random_instance_file.tsp')
-        elif alg == "k-NN":
-            t = nearestNeigbor2('TSP_Data/random_instance_file.tsp')
-        elif alg == "k-random":
-            pass
-        elif alg == "2opt":
-            pass
-        else:
-            sys.exit(1)
-        t.write_info()
-        t.run()
-        t.write_results()
-elif len(sys.argv) == 2:
 
+    file = 'TSP_Data/random_instance_file.tsp'
+    instance = ReadData(file)
+    size = instance.size
+    dis_mat = instance.GetDistanceMat()
+    write_info()
+    print(dis_mat)
     while True:
         alg = input("NN, k-NN, k-random, 2opt, or anything else to quit ")
         if alg == "NN":
-            t = nearestNeighbour(sys.argv[1])
+            path = nearestNeighbour.run(size, dis_mat)
+            print(path)
         elif alg == "k-NN":
-            t = nearestNeigbor2(sys.argv[1])
+            path = nearestNeigbor2.run(size, dis_mat)
         elif alg == "k-random":
             pass
         elif alg == "2opt":
             pass
         else:
             sys.exit(1)
-        t.write_info()
-        t.run()
-        t.write_results()
+        write_results()
+elif len(sys.argv) == 2:
+    file = sys.argv[1]
+    instance = ReadData(file)
+    size = instance.size
+    dis_mat = instance.GetDistanceMat()
+    write_info()
+    while True:
+        alg = input("NN, k-NN, k-random, 2opt, or anything else to quit ")
+        if alg == "NN":
+            path = nearestNeighbour.run(size, dis_mat)
+        elif alg == "k-NN":
+            path = nearestNeigbor2.run(size, dis_mat)
+
+        elif alg == "k-random":
+            pass
+        elif alg == "2opt":
+            pass
+        else:
+            sys.exit(1)
+        write_results()
+
 
 
 
--- nearestNeighbour.py
+++ nearestNeighbour.py
@@ -7,63 +7,27 @@ import calcTour
 import random
 start_time = time.time()
 
-class nearestNeighbour():
-
-    def __init__(self, file):
-
-        self.file = file
-        self.instance = ReadData(self.file)
-        self.size = self.instance.size
-        self.dis_mat = self.instance.GetDistanceMat()
-
-        self.path =[]
-        self.cities = []
-
-    def write_info(self):
-        """
-        write info about instance
-        """
-        print("Instance name:", self.instance.name)
-        print("Dimention:", self.size)
-        print("Distance Type:", self.instance.EdgeWeightType)
-        # displayTour.matPrint(self.dis_mat)
-
-    def run(self):
-        start_point = random.randint(0, self.size-1)
-        path_index = [start_point]
-        changed_path = copy.copy(self.dis_mat)
-        for i in range(self.size):
-            changed_path[i][i] = 999999999
-
-        path_index.append( np.argmin(changed_path[start_point]))
-        for i in range(self.size):
-            changed_path[i][start_point] = 999999999
-
-        distance = self.dis_mat[start_point][path_index[1]]
-        i = 2
-
-        while i != self.size:
-            last_element = path_index[-1]
-            # zmiana ostatnio użytego elementu na maksymalny, żeby nie został użyty
-            for j in changed_path:
-                j[last_element] = 999999999
-
-            path_index.append( np.argmin(changed_path[last_element]))
-            distance += self.dis_mat[last_element][path_index[-1]]
-
-            i += 1
-
-        path_index = [1 + x for x in path_index]
-        self.path = path_index
-
-    def write_results(self):
-        # ścieżka
-        displayTour.printPath(self.dis_mat,self.path)
-        # dystans
-        print("rozwiązanie: ", calcTour.fc(self.dis_mat, self.path))
-        # wykres
-        if self.instance.getEdgeWeightType() == "EUC_2D":
-            displayTour.EUCgraph(self.instance, self.path)
+def run(size,dis_mat):
+    start_point = random.randint(0, size-1)
+    path_index = [start_point]
+    changed_path = copy.copy(dis_mat)
+    for i in range(size):
+        changed_path[i][i] = 999999999
+    path_index.append( np.argmin(changed_path[start_point]))
+    for i in range(size):
+        changed_path[i][start_point] = 999999999
+    distance = dis_mat[start_point][path_index[1]]
+    i = 2
+    while i != size:
+        last_element = path_index[-1]
+        # zmiana ostatnio użytego elementu na maksymalny, żeby nie został użyty
+        for j in changed_path:
+            j[last_element] = 999999999
+        path_index.append( np.argmin(changed_path[last_element]))
+        distance += dis_mat[last_element][path_index[-1]]
+        i += 1
+    path_index = [1 + x for x in path_index]
+    return path_index
 
 
 
