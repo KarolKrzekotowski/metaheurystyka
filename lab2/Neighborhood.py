92ad710db55389038c5727beb565cedea1b1e5b7
Szymon Pilawka <spilawka@gmail.com>
2022-04-20 19:13:21 +0200

    sąsiedztwo

--- lab2/Neighborhood.py
+++ lab2/Neighborhood.py
@@ -1,2 +1,40 @@
-def get(bestCandidate):
-    pass
+import Paths
+import random
+
+class Neighborhood:
+    def __init__(self,pathSize):
+        self.pathSize = pathSize
+        #do dostrojenia
+        self.hoodSize = 100
+        self.stallLimit = 5
+
+    def get(self,bestCandidate,func):
+        #słownik sprawdza czy nie wykonaliśmy już danego inverta 
+        dict = {}
+        arr = []
+        stall = 0
+        for i in range(0,self.hoodSize):
+            r1 = random.randint(0,self.pathSize-1)
+            while(True):
+                r2 = random.randint(0,self.pathSize-1)
+                if r1 != r2: break
+            if r1>r2:
+                (r1,r2)=(r2,r1)
+            #klucz unikalny dla każdej pary
+            key = r1 + r2*self.pathSize
+            
+            if key in dict:
+                #potwórz krok
+                i=i-1
+                stall=stall+1
+                #wyjdź z utknięcia - przerwij pętlę
+                if (stall >= self.stallLimit):
+                    break
+                continue
+
+            stall = 0
+            dict[key]=True
+            newPath = func(bestCandidate,r1,r2)
+            arr.append(newPath)
+
+        return arr
--- lab2/TabuSearch.py
+++ lab2/TabuSearch.py
@@ -2,8 +2,9 @@ import tsplib95
 import numpy as np
 import copy
 import sys
-sys.path.insert()
+#sys.path.insert()
 import readData
+import Neighborhood
 import nearestNeigbor2
 from Paths import fc, invert
 
@@ -16,24 +17,27 @@ class TabuSearch:
         self.path = path
         self.bestdistance = bestdistance
         self.dis_mat = dis_mat
+        self.NH = Neighborhood.Neighborhood(len(self.path))
 
     def run(self, max_iterations):
         sBest = copy.copy(self.path)
         bestCandidate = copy.copy(self.path)
+        print(fc(self.dis_mat,bestCandidate))
         tabuList = []
         tabuList.append(sBest)
         x = 0
         while x < max_iterations:
-            sNeighborhood = getNeighbors(bestCandidate)
+            sNeighborhood = self.NH.get(bestCandidate,invert)
             bestCandidate = sNeighborhood[0]
             for sCandidate in sNeighborhood:
-                if  sCandidate not in tabuList and fc(self.dis_mat, sCandidate) > fc(self.dis_mat, bestCandidate):
+                if  sCandidate not in tabuList and fc(self.dis_mat, sCandidate) < fc(self.dis_mat, bestCandidate):
                     bestCandidate = sCandidate
-            if fc(self.dis_mat,bestCandidate) > fc(sBest):
+            if fc(self.dis_mat,bestCandidate) < fc(self.dis_mat,sBest):
                 sBest = bestCandidate
             tabuList.append(bestCandidate)
+            print("best of",x,":",fc(self.dis_mat,sBest))
             x += 1
-        print(fc(sBest))
+        print(fc(self.dis_mat,sBest))
 
 
 
